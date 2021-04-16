fb5576306c686aef5389e2ef52dbbc8808537b9e
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2021-04-16 15:18:11 +0000

    deployment part 2

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7)kkk$^nkl3(x2$+!hdkwr-ojq!6b3jw+)p%i)c-z!7b%wl&6w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://titan-strength.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -104,12 +104,17 @@ WSGI_APPLICATION = 'milestone_4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
