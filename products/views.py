f8d9f3aca34b42a407aad135646e7d687de07218
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2021-04-28 19:28:23 +0000

    finished current sorting functionality for sorting box

--- products/views.py
+++ products/views.py
@@ -51,6 +51,7 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
