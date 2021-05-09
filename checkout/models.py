795b2227fab444ea822e8693bebb0b3254377af1
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2021-05-09 20:10:13 +0000

    finished checkout success page and fixed a duplicate function name on checkout signals.py

--- checkout/models.py
+++ checkout/models.py
@@ -31,7 +31,7 @@ class Order(models.Model):
     def update_total(self):
         # updates the grand total
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
-            'lineitem_total__sum']
+            'lineitem_total__sum'] or 0
         self.delivery_cost = self.order_total * \
             settings.STANDARD_DELIVERY_PERCENTAGE / 100
 
