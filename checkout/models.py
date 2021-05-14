3aad7aa2ef3adf2c759f47e018a3991a5e16718f
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2021-05-14 15:05:56 +0000

    staging, finished webhooks for checkout, added countries dropdown to checkout form, created profile app

--- checkout/models.py
+++ checkout/models.py
@@ -3,14 +3,18 @@ from django.conf import settings
 from django.db import models
 from django.db.models import Sum
 from products.models import Product
+from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
