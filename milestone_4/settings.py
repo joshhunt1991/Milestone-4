082724bcda3fa3d7cebaa429885fbd102bb4ff35
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2021-05-28 15:45:06 +0000

    moved secret key from settings.py into env.py file

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from os import path
+
+if path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,9 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-7)kkk$^nkl3(x2$+!hdkwr-ojq!6b3jw+)p%i)c-z!7b%wl&6w'
-)
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
